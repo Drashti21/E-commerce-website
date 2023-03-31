597ff7f2159eb3a8a6e5cc7afc961225652c91d5
Drashti21 <68647147+Drashti21@users.noreply.github.com>
2023-03-31 16:04:01 +0530

    commit initial files

---
+++ script.js
@@ -0,0 +1,80 @@
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.head .flex .navbar');
+
+menuBtn.onclick = () =>{
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+var swiper = new Swiper(".course-slider", {
+  spaceBetween: 20,
+  grabCursor:true,
+  loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+      
+      },
+      1024: {
+        slidesPerView: 3,
+        
+      },
+    },
+});
+
+
+var swiper = new Swiper(".teachers-slider", {
+  spaceBetween: 20,
+  grabCursor:true,
+  loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+      
+      },
+      1024: {
+        slidesPerView: 3,
+        
+      },
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  grabCursor:true,
+  loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+      
+      },
+      1024: {
+        slidesPerView: 3,
+        
+      },
+    },
+});
